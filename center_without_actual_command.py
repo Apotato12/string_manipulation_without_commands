530c444ea8fa698bc445a872606eee40178970c8
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:31:34 +0800

    added_PEP8_standards_and_comments

--- center_without_actual_command.py
+++ center_without_actual_command.py
@@ -1,13 +1,19 @@
-input_string = input("Enter a string: ")
-width = int(input("Enter the desired width: "))
+# Program 7
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def center():
+    """Adds space characters at the beginning and at the end of the string to center it within the specified width without using center() function."""
+    input_string = input("Enter a string: ")
+    width = int(input("Enter the desired width: "))
 
-total_spaces = max(0, width - len(input_string))
-    
-left_padding = total_spaces // 2
+    total_spaces = max(0, width - len(input_string))
 
-right_padding = total_spaces - left_padding
+    left_padding = total_spaces // 2
 
-result = ' ' * left_padding + input_string + ' ' * right_padding
+    right_padding = total_spaces - left_padding
 
-print("Result:", result)
+    result = ' ' * left_padding + input_string + ' ' * right_padding
 
+    print("Result:", result)
+
+center()
