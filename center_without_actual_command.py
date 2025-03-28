7309fbb480945bec0fb6b93cb3897ed22834a3af
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:30:33 +0800

    working_code

--- center_without_actual_command.py
+++ center_without_actual_command.py
@@ -1,5 +1,13 @@
-#prompt the user to enter the a sentence
-#ask the user to input a certain width
-#check how many spaces to be added
-#add spaces to both left and right
-#print the result
+input_string = input("Enter a string: ")
+width = int(input("Enter the desired width: "))
+
+total_spaces = max(0, width - len(input_string))
+    
+left_padding = total_spaces // 2
+
+right_padding = total_spaces - left_padding
+
+result = ' ' * left_padding + input_string + ' ' * right_padding
+
+print("Result:", result)
+
