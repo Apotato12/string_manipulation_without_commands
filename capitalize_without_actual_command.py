236334910ea85a9c830bb8d5f30c469d9c5f7391
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:37:55 +0800

    pseudocode

---
+++ capitalize_without_actual_command.py
@@ -0,0 +1,4 @@
+#prompt the user to enter a sentence
+#check if the first letter of the sentence is lowercase and if the other are uppercase
+#if the first letter is lowercase convert it to uppercase and the other to lowercase
+#print the output
