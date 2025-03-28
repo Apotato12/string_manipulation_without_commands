d4980fd516e7b8dade91682797691687e4497643
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 20:31:34 +0800

    pseudocode

---
+++ lstrip_without_actual_command.py
@@ -0,0 +1,4 @@
+#prompt the user to input his name with spaces in the front
+#create a index to look for the first letter that is not a space
+#once the letter is found slice the string to remove the space
+#print the name
