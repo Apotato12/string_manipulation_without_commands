97c78a69d49f3c882eea2f4d0a3d48d53e4ede85
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 21:59:11 +0800

    added_PEP8_standards_and_added_comments

--- isupper_without_actual_command.py
+++ isupper_without_actual_command.py
@@ -1,12 +1,20 @@
-input_string = input("Enter a string: ")
+# Program 4
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def check_all():
+    """Checks if all characters of the string are uppercase without using the isupper() function."""
+    input_string = input("Enter a string: ")
 
-all_upper = True
+    all_upper = True
 
-for char in input_string:
-    if not ('A' <= char <= 'Z'):
-        all_upper = False
+    for char in input_string:
+        if not ('A' <= char <= 'Z'):
+            all_upper = False
+            break
 
     if all_upper:
-        print("this characters are uppercase.")
+        print("All characters are uppercase.")
     else:
         print("Not all characters are uppercase.")
+
+check_all()
