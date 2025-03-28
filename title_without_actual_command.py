fc430f50931223fecd22e5bbe4bb043139f71e86
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:46:39 +0800

    working_code

--- title_without_actual_command.py
+++ title_without_actual_command.py
@@ -1,4 +1,18 @@
-#prompt the user to enter a sentece 
-#split the sentence into words
-#capitalize the first letter of each word
-#print the new sentence
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+    
+title = []
+
+for word in words:
+
+    if len(word) > 0:
+        title_cased = word[0].upper() + word[1:].lower()
+        title.append(title_cased)
+
+    else:
+        title.append(word) 
+
+result = ' '.join(title)
+
+print("Result:", result)
