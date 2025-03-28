36ad923d62ba2ca90f5f0dbd138e3d8488b3cdbc
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 21:04:41 +0800

    working_code

--- removeprefix_without_actual_command.py
+++ removeprefix_without_actual_command.py
@@ -1,6 +1,9 @@
-#prompt the user to enter a sentence
-#ask the user which prefix to remove
-#check if the input starts with the prefix
-#if it does, slice the prefix off of the input
-#if it does not. leave it as it is
-#print the new sentence
+input_string = input("Enter a string: ")
+prefix = input("Enter the prefix to remove: ")
+
+if input_string.startswith(prefix):
+    result = input_string[len(prefix):]
+else:
+    result = input_string
+
+print("Result:", result)
