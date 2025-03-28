c7917cc9c58f38de5b3115dd6f5e0c0b41edd1d4
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:11:21 +0800

    working_code

--- endswith_without_actual_command.py
+++ endswith_without_actual_command.py
@@ -1,5 +1,10 @@
-#ask the user to input a sentence
-#ask the user to input a suffix
-#check if that sentence ends with a specific word
-#if it does, print "it edn with 'word'"
-#else print "it does not end with 'word'""
+input_sentence = input("Enter a string: ")
+suffix = input("Enter the suffix to check: ")
+
+suffix_length = len(suffix)
+    
+if len(input_sentence) >= suffix_length:
+    if input_sentence[-suffix_length:] == suffix:
+        print("The string ends with the specified suffix.")
+    else:
+        print("The string does not end with the specified suffix.")
