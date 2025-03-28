ab7de377e243052aa6f7cf46d0160843ddc7583b
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 20:59:40 +0800

    pseudocode

---
+++ removeprefix_without_actual_command.py
@@ -0,0 +1,6 @@
+#prompt the user to enter a sentence
+#ask the user which prefix to remove
+#check if the input starts with the prefix
+#if it does, slice the prefix off of the input
+#if it does not. leave it as it is
+#print the new sentence
