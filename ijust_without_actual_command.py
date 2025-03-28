0aeb768fb23247e60950e8fdb10e83f65c3d9f97
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:28:01 +0800

    followed_PEP8_and_added_comments

--- ijust_without_actual_command.py
+++ ijust_without_actual_command.py
@@ -1,6 +1,13 @@
-input_string = input("Enter a string: ")
-width = int(input("Enter the desired width: "))
+# Program 6
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def ljust():
+    """Adds space characters at the end of the string to reach the specified width without using ljust() function."""
+    input_string = input("Enter a string: ")
+    width = int(input("Enter the desired width: "))
 
-result = input_string + ' ' * max(0, width - len(input_string))
+    result = input_string + ' ' * max(0, width - len(input_string))
 
-print("Result:", result)
+    print("Result:", result)
+
+ljust()
