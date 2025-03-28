8665b2e5e329be11f0997ee012f364c7125f5cf4
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 21:16:16 +0800

    added_PEP8_standards_and_comments

--- lower_without_actual_command.py
+++ lower_without_actual_command.py
@@ -1,11 +1,18 @@
-input_string = input("Enter a string: ")
-
-result = ""
-
-for char in input_string:
+# Program 3
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def capital_to_lowercase():
+    """Converts all characters of the string into lowercase without using the lower() function."""
+    input_string = input("Enter a string: ")
+    
+    result = ""
+    
+    for char in input_string:
         if 'A' <= char <= 'Z':
             result += chr(ord(char) + 32)
         else:
             result += char
 
-print(result)
+    print("String in lowercase:", result)
+
+capital_to_lowercase()
