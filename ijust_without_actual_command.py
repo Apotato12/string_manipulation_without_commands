09931d9669b009e4a114d715565dcb125e2e14de
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:26:48 +0800

    changed_working_code_from_rjust_to_ljust

--- ijust_without_actual_command.py
+++ ijust_without_actual_command.py
@@ -1,7 +1,6 @@
 input_string = input("Enter a string: ")
 width = int(input("Enter the desired width: "))
 
-fillchar = ' '
-result = fillchar * max(0, width - len(input_string)) + input_string
+result = input_string + ' ' * max(0, width - len(input_string))
 
 print("Result:", result)
