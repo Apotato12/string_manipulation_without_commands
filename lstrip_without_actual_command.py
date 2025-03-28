7d143e6c73a8d8d61f5397db5920075c05eb96d0
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 20:39:44 +0800

    followed_PEP8_Standards_and_added_comments

--- lstrip_without_actual_command.py
+++ lstrip_without_actual_command.py
@@ -1,8 +1,15 @@
-input_name = input("enter your name: ")
+# Program 1
+# Atienza, Rein Gabriel
+# Bscpe 1-2
+def no_lstrip():
+    """Allows the user to input a name and prints it without leading spaces."""
+    input_name = input("Enter your name: ")
 
-index = 0
+    index = 0
 
-while index < len(input_name) and input_name[index] == ' ':
+    while index < len(input_name) and input_name[index] == ' ':
         index += 1
 
-print (input_name[index:]) 
+    print(input_name[index:]) 
+
+no_lstrip()
