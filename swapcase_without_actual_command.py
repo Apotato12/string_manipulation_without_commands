525b63a75d1a39d504a41a81a404c6e06af721ff
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:35:25 +0800

    added_PEP8_format_and_comments

--- swapcase_without_actual_command.py
+++ swapcase_without_actual_command.py
@@ -1,13 +1,22 @@
-input_string = input("Enter a string: ")
+# Program 8
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def swapcase():
+    """Reverses the casing of each character in the string without using swapcase() function."""
+    input_string = input("Enter a string: ")
     
-result = ""
+    result = ""
     
-for char in input_string:
-    if 'a' <= char <= 'z': 
-            result += char.upper() 
-    elif 'A' <= char <= 'Z': 
-            result += char.lower()
-    else:
-            result += char
+    for char in input_string:
 
-print("Result:", result)
+        if 'a' <= char <= 'z': 
+            result += char.upper() 
+
+        elif 'A' <= char <= 'Z': 
+            result += char.lower() 
+        else:
+            result += char 
+
+    print("Result:", result)
+
+swapcase()
