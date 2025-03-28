f05e4bc8b0e35b67d51c745fea0fa23fc377689e
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:13:34 +0800

    followed_PEP8_standards_and_added_comments

--- endswith_without_actual_command.py
+++ endswith_without_actual_command.py
@@ -1,10 +1,17 @@
-input_sentence = input("Enter a string: ")
-suffix = input("Enter the suffix to check: ")
+# Program 8
+# Atienza, Rein Gabriel
+# BSCPE 1-2
+def ends_with():
+    """Checks if the string ends with the specified suffix without using the endswith() function."""
+    input_sentence = input("Enter a string: ")
+    suffix = input("Enter the suffix to check: ")
 
-suffix_length = len(suffix)
-    
-if len(input_sentence) >= suffix_length:
-    if input_sentence[-suffix_length:] == suffix:
-        print("The string ends with the specified suffix.")
-    else:
-        print("The string does not end with the specified suffix.")
+    suffix_length = len(suffix)
+
+    if len(input_sentence) >= suffix_length:
+        if input_sentence[-suffix_length:] == suffix:
+            print("The string ends with the specified suffix.")
+        else:
+            print("The string does not end with the specified suffix.")
+
+eInds_with()
