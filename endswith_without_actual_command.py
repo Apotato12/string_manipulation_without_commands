3cbba963fb2ad2b98f19545bd06054c790e0d209
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:08:31 +0800

    pseudocode

---
+++ endswith_without_actual_command.py
@@ -0,0 +1,5 @@
+#ask the user to input a sentence
+#ask the user to input a suffix
+#check if that sentence ends with a specific word
+#if it does, print "it edn with 'word'"
+#else print "it does not end with 'word'""
