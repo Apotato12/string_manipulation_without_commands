3eca7a0bbf6e00277b6b6879c7d535c8a7b138ca
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:47:45 +0800

    followed_PEP8_standards_and_added_comments

--- title_without_actual_command.py
+++ title_without_actual_command.py
@@ -1,18 +1,19 @@
-sentence = input("Enter a sentence: ")
+# Program 10
+def custom_title():
+    """Capitalizes the first letter of each word in the string and makes all other letters lowercase without using title() function."""
+    sentence = input("Enter a sentence: ")
 
-words = sentence.split()
-    
-title = []
+    words = sentence.split()
+    title = [] 
 
-for word in words:
+    for word in words:
+        if len(word) > 0: 
+            title_cased = word[0].upper() + word[1:].lower()
+            title.append(title_cased) 
+        else:
+            title.append(word)
+    result = ' '.join(title) 
 
-    if len(word) > 0:
-        title_cased = word[0].upper() + word[1:].lower()
-        title.append(title_cased)
+    print("Result:", result)
 
-    else:
-        title.append(word) 
-
-result = ' '.join(title)
-
-print("Result:", result)
+custom_title()
