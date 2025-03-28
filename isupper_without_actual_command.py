6e1bfeea0a905ec1abbdc5424e36cd4ddeff6d92
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 21:55:49 +0800

    working_code

--- isupper_without_actual_command.py
+++ isupper_without_actual_command.py
@@ -1,4 +1,12 @@
-#prompt the user for a sentence
-#check if the program is all upercase
-#if yes print "ALL UPPERCASE"
-#if no print "not all uppercase"
+input_string = input("Enter a string: ")
+
+all_upper = True
+
+for char in input_string:
+    if not ('A' <= char <= 'Z'):
+        all_upper = False
+
+    if all_upper:
+        print("this characters are uppercase.")
+    else:
+        print("Not all characters are uppercase.")
