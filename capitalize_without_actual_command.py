994265a11d9915691956345b110953a9b10f427a
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-28 23:41:16 +0800

    followed_PEP8_standards_and_added_comments

--- capitalize_without_actual_command.py
+++ capitalize_without_actual_command.py
@@ -1,8 +1,15 @@
-sentence = input("Enter a sentence: ")
+# Program 9
+# Atienza, Rein Gabriel
+# BSPCE 1-2
+def capitalize():
+    """Capitalizes the first letter of the string and makes all other letters lowercase without using capitalize() function."""
+    sentence = input("Enter a sentence: ")
 
-if len(sentence) == 0:
-        result = sentence 
-else:
+    if len(sentence) == 0:
+        result = sentence
+    else:
         result = sentence[0].upper() + sentence[1:].lower()
 
-print("Result:", result)
+    print("Result:", result)
+
+capitalize()
