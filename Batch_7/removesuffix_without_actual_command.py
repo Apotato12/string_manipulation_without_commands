915b52bc3274eb753fee27c5715018a5debf7544
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-31 19:32:33 +0800

    working_code

--- Batch_7/removesuffix_without_actual_command.py
+++ Batch_7/removesuffix_without_actual_command.py
@@ -1,5 +1,9 @@
-#prompt the user to enter a sentence
-#prompt the user to enter a word to remove from the end of their sentence
-#check if the word is in the sentence
-#if it is remove it from the end of the sentence
-#if it's not print the message as it is
+user_input = input("Please enter a string: ")
+suffix = input("Please enter the suffix to remove: ")
+
+if user_input.endswith(suffix):
+    result = user_input[:-len(suffix)]
+else:
+    result = user_input
+
+print("Result:" + result)
