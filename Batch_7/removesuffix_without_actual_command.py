3328013a6b84c540c4f785591e6455b31245da36
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-31 19:29:14 +0800

    pseudocode

---
+++ Batch_7/removesuffix_without_actual_command.py
@@ -0,0 +1,5 @@
+#prompt the user to enter a sentence
+#prompt the user to enter a word to remove from the end of their sentence
+#check if the word is in the sentence
+#if it is remove it from the end of the sentence
+#if it's not print the message as it is
