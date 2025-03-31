4bff4e18f9542cf2dc5bc323438af009183c9adb
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-31 19:33:38 +0800

    followed_PEP8_commands_and_added_comments

--- Batch_7/removesuffix_without_actual_command.py
+++ Batch_7/removesuffix_without_actual_command.py
@@ -1,9 +1,18 @@
-user_input = input("Please enter a string: ")
-suffix = input("Please enter the suffix to remove: ")
+# Program 2
+# Atienza, Rein Gabriel
+# BSPCE 1-2
 
-if user_input.endswith(suffix):
-    result = user_input[:-len(suffix)]
-else:
-    result = user_input
+def removesuffix():
+    """Removes the specified suffix from the end of the input string if it exists."""
+    user_input = input("Please enter a string: ")
+    
+    suffix = input("Please enter the suffix to remove: ")
 
-print("Result:" + result)
+    if user_input.endswith(suffix):
+        result = user_input[:-len(suffix)]
+    else:
+        result = user_input 
+
+    print("Result:", result)
+
+removesuffix()
