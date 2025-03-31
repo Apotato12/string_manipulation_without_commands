158311033cd6a946a4a12c96b105d1bbaaef2248
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-31 19:12:52 +0800

    followed_PEP8_Standards_and_added_comments

--- Batch_7/rstrip_without_actual_command.py
+++ Batch_7/rstrip_without_actual_command.py
@@ -1,12 +1,20 @@
-user_input = input("Please enter a string: ")
+# Program 1
+# Atienza, Rein Gabriel
+# BSPCE 1-2
 
-index = len(user_input) - 1
+def r_strip():
+    """removes the end spaces from the end of the input string and returns the modified string."""
+    user_input = input("Please enter a string: ")
 
-while index >= 0:
-    if user_input[index] != ' ' :
-        break
-    index -= 1
+    index = len(user_input) - 1
 
-result = user_input[:index + 1]
+    while index >= 0:
+        if user_input[index] != ' ':
+            break
+        index -= 1
 
-print("result: " + result)
+    result = user_input[:index + 1]
+
+    print("Result:", result)
+
+r_strip()
