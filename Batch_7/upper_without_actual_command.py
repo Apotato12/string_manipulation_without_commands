0e4028ceaf437a9fef967db69ce0e20187b23e27
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-03-31 20:53:31 +0800

    added_PEP8_command_and_comments

--- Batch_7/upper_without_actual_command.py
+++ Batch_7/upper_without_actual_command.py
@@ -1,12 +1,22 @@
-user_input = input("Please enter a sentence: ")
-capitalize = ""
+# Program 3
+# Atienza, Rein Gabriel
+# BSPCE 1-2
 
-for char in user_input:
-        
-    if 'a' <= char <= 'z':
-        capitalize += chr(ord(char) - 32)
+def uppercase():
+    """Converts all characters of the input string to uppercase without using the upper() function."""
+    user_input = input("Please enter a sentence: ")
+    
+    capitalize = ""
 
-    else:
-        capitalize += char
+    for char in user_input:
+        if 'a' <= char <= 'z':
 
-print("Result:" + capitalize)
+            capitalize += chr(ord(char) - 32)
+
+        else:
+
+            capitalize += char
+
+    print("Result: " + capitalize)
+
+uppercase()
