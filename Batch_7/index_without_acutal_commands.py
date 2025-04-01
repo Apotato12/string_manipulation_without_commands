1aca3ab7b363a0e8b5471fffab56e548e11f82b2
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 15:28:07 +0800

    working_code

--- Batch_7/index_without_acutal_commands.py
+++ Batch_7/index_without_acutal_commands.py
@@ -1,5 +1,12 @@
-#ask the user to input a sentence
-#initialize the index 
-#look for the word in the sentence
-#if it's not found print "not found"
-#if found print "found"
+user_input = input("Please enter a string: ")
+word = input("Please enter the word to find: ")
+
+index = -1
+
+for i in range(len(user_input) - len(word) + 1):
+        if user_input[i:i + len(word)] == word:
+
+            print(f"The word '{word}' is found at index {i}.")
+            break
+        else:
+              print("the word is not found")
