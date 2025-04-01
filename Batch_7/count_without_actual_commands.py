be7fb6b0fdb0babbe2dd60dfb4128577c0180e8d
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 14:54:05 +0800

    added_PEP8_and_comments

--- Batch_7/count_without_actual_commands.py
+++ Batch_7/count_without_actual_commands.py
@@ -1,12 +1,20 @@
-user_input = input("Please enter a string: ")
-word = input("Please enter the word to count: ")
+# Program 8
+# Atienza, Rein Gabriel
+# BSPCE 1-2
+def word_count():
+    """Counts how many times a specified word appears in the input sentence without using count()."""
+    user_input = input("Please enter a string: ")
+    word = input("Please enter the word to count: ")
 
-count = 0
+    count = 0
 
 
-for i in range(len(user_input) - len(word) + 1):
-        if user_input[i:i + len(word)] == word:
-            count += 1
+    for i in range(len(user_input) - len(word) + 1):
+            if user_input[i:i + len(word)] == word:
+                count += 1
 
 
-print("the word appears " + str(count) + " times")
+    print("the word appears " + str(count) + " times")
+
+
+word_count()
