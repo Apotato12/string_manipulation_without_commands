2f5e9e260c4d0b13b5175119b6a7d15abae3729c
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 15:29:31 +0800

    added_PEP8_format_and_added_comments

--- Batch_7/index_without_acutal_commands.py
+++ Batch_7/index_without_acutal_commands.py
@@ -1,12 +1,18 @@
-user_input = input("Please enter a string: ")
-word = input("Please enter the word to find: ")
+# Program 9
+# Atienza, Rein Gabriel
+# BSPCE 1-2
+def index():
+    user_input = input("Please enter a string: ")
+    word = input("Please enter the word to find: ")
 
-index = -1
+    index = -1
 
-for i in range(len(user_input) - len(word) + 1):
-        if user_input[i:i + len(word)] == word:
+    for i in range(len(user_input) - len(word) + 1):
+            if user_input[i:i + len(word)] == word:
 
-            print(f"The word '{word}' is found at index {i}.")
-            break
-        else:
-              print("the word is not found")
+                print(f"The word '{word}' is found at index {i}.")
+                break
+            else:
+                print("the word is not found")
+
+index()
