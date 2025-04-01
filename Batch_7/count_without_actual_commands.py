8e885001f32421b1ecc7583ad2821df6a1853ad7
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 14:27:47 +0800

    pseudocode

---
+++ Batch_7/count_without_actual_commands.py
@@ -0,0 +1,4 @@
+#ask the user to input a sentence
+#ask the user to input a word
+#check if the word is present in the sentence and count how many times it shows up
+#print the number of times it showed up
