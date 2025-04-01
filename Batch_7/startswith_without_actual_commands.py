92e88e548ac907541edef908a24e7c8eb07e7692
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 12:27:25 +0800

    followed_PEP8_standards_and_added_comments

--- Batch_7/startswith_without_actual_commands.py
+++ Batch_7/startswith_without_actual_commands.py
@@ -1,8 +1,19 @@
-sentence = input("Please enter a string: ")
-word = input("Please enter the word to check: ")
+# Program 5
+# Atienza, Rein Gabriel
+# BSPCE 1-2
 
-for i in range(len(word)):
-        if sentence[i] != word[i]:
-            print("The string does not start with the specified prefix.")
-        else:
-            print("The string starts with the specified prefix.")
+def starts_with():
+    """Checks if the input string starts with the specified prefix without using the startswith() function."""
+    sentence = input("Please enter a string: ")
+
+    word = input("Please enter the word to check: ")
+
+    for i in range(len(word)):
+            
+            if sentence[i] != word[i]:
+                print("The string does not start with the specified prefix.")
+
+            else:
+                print("The string starts with the specified prefix.")
+
+starts_with()
