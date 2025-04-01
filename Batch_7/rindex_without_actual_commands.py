d83c3418a440ed2621510641e670d3b8241558f0
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 15:39:01 +0800

    working_code

--- Batch_7/rindex_without_actual_commands.py
+++ Batch_7/rindex_without_actual_commands.py
@@ -1,4 +1,7 @@
-#prompt the user to add a sentence
-#prompt the user to enter a word
-#find the word in the sentence starting from the right
-#print the index where the word is found and the word itself
+user_input = input("Please enter a string: ")
+word = input("Please enter the substring to find: ")
+
+
+for i in range(len(user_input) - len(word), -1, -1):
+        if user_input[i:i + len(word)] == word:
+            print(f"The word '{word}' is found at index {i}.")
