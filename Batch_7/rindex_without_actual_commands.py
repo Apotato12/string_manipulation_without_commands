c08457f2d9d4e0912750326c4f72b8c28c6398d2
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 15:35:08 +0800

    pseudocode

---
+++ Batch_7/rindex_without_actual_commands.py
@@ -0,0 +1,4 @@
+#prompt the user to add a sentence
+#prompt the user to enter a word
+#find the word in the sentence starting from the right
+#print the index where the word is found and the word itself
