11f87f0746d15f381250d67cd2cf78a30342cf9e
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 15:41:57 +0800

    added_PEP8_and_comments

--- Batch_7/rindex_without_actual_commands.py
+++ Batch_7/rindex_without_actual_commands.py
@@ -1,7 +1,14 @@
-user_input = input("Please enter a string: ")
-word = input("Please enter the substring to find: ")
+# Program 10
+# Atienza, Rein Gabriel
+# BSPCE 1-2
+def rindex():
+    """Finds the last index of a specified word in the input string without using rindex()."""
+    user_input = input("Please enter a string: ")
+    word = input("Please enter the word to find: ")
 
 
-for i in range(len(user_input) - len(word), -1, -1):
-        if user_input[i:i + len(word)] == word:
-            print(f"The word '{word}' is found at index {i}.")
+    for i in range(len(user_input) - len(word), -1, -1):
+            if user_input[i:i + len(word)] == word:
+                print(f"The word '{word}' is found at index {i}.")
+
+rindex()
