13d93d8c7f8a91117746d298fdad02625615e880
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 12:22:19 +0800

    pseudocode

---
+++ Batch_7/startswith_without_actual_commands.py
@@ -0,0 +1,5 @@
+#prompt the user to enter a sentence
+#ask for a word to check if it starts with a certain sentence
+#compare if the word starts with the sentence
+#if it does print "it starts with the word"
+#if not print "it does not start with the word"
