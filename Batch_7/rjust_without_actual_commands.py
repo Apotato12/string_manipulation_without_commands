e6c22ea5d5e1b542b1ccc73bf2607b50da38cac5
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 12:41:15 +0800

    followed_PEP8_standards_and_added_comments

--- Batch_7/rjust_without_actual_commands.py
+++ Batch_7/rjust_without_actual_commands.py
@@ -1,14 +1,23 @@
-user_input = input("Please enter a string: ")
-width = int(input("Please enter the total width: "))
+# Program 6
+# Atienza, Rein Gabriel
+# BSPCE 1-2
 
-spaces = width - len(user_input)
+def r_justify():
+    """Adds space characters at the beginning of the string to reach the specified length without using rjust()."""
+    user_input = input("Please enter a string: ")
+    
+    width = int(input("Please enter the total width: "))
 
-if spaces > 0:
-        
-        justified_string = ' ' * spaces + user_input
+    spaces = width - len(user_input)
 
-else:
-        
-        justified_string = user_input
+    if spaces > 0:
+            
+            justified_string = ' ' * spaces + user_input
 
-print("Justified string:", justified_string)
+    else:
+            
+            justified_string = user_input
+
+    print("Justified string:", justified_string)
+
+r_justify()
