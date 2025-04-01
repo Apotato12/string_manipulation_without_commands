aa1281ac0f0f95919eda372d1371e182e6eddc0b
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 12:35:26 +0800

    pseudocode

---
+++ Batch_7/rjust_without_actual_commands.py
@@ -0,0 +1,6 @@
+#propmt the user to enter a sentence
+#ask the user to enter a width
+#check if the amount of spaces is appropriate for the width
+#if no enough spaces add spaces
+#if yes change nothing
+#print the sentence
