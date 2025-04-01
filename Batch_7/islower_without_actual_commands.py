233daa5e1241f6d317d56cc8337d63422be1ae6b
Apotato12 <atienza.reingabriel308129@gmail.com>
2025-04-01 12:16:46 +0800

    added_PEP8_standards_and_inserted_comments

--- Batch_7/islower_without_actual_commands.py
+++ Batch_7/islower_without_actual_commands.py
@@ -1,9 +1,19 @@
-user_input = input("Please enter a sentence: ")
+# Program 4
+# Atienza, Rein Gabriel
+# BSPCE 1-2
 
-for char in user_input:
+def iflowercase():
+    """Checks if all characters in the input string are lowercase without using the islower() function."""
+    user_input = input("Please enter a sentence: ")
+
+    for char in user_input:
         if ('a' <= char <= 'z'):
+
             print("all characthers are lowercase.")
             
         else:
+
             print("all characthers are not lowercase.")
             break
+
+iflowercase()
